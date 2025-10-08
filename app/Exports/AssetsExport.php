2dbb28e84a434999153e09dfd3193f217f94299b
Crolweb <crolweb@gmail.com>
2025-10-08 16:57:13 +0200

    remove events in AssetsExport as it is not necessary (used in AssetDataExport)

--- app/Exports/AssetsExport.php
+++ app/Exports/AssetsExport.php
@@ -8,10 +8,8 @@ use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Maatwebsite\Excel\Concerns\RegistersEventListeners;
 
-class AssetsExport implements WithMultipleSheets, WithEvents
+class AssetsExport implements WithMultipleSheets
 {
-    use RegistersEventListeners;
-
     public function sheets() : array
     {
         return [
@@ -20,47 +18,4 @@ class AssetsExport implements WithMultipleSheets, WithEvents
         ];
     }
 
-    public static function afterSheet(AfterSheet $event)
-    {
-        \Log::info('AFTER SHEET');
-        \Log::info($event->sheet->getTitle());
-        if ($event->sheet->getTitle() === 'Datas') {
-            $spreadsheet = $event->sheet->getParent();
-            // \Log::info($spreadsheet);
-            $dataSheet = $spreadsheet->getSheetByName('Datas');
-            // \Log::info($dataSheet);
-
-            // Syntaxe alternative plus explicite
-            $namedRange = new \PhpOffice\PhpSpreadsheet\NamedRange(
-                'sites',
-                $dataSheet,
-                'A2:A50' // Sans les $
-            );
-
-            \Log::info($namedRange);
-
-            $spreadsheet->addNamedRange($namedRange);
-
-            $spreadsheet->addDefinedName(
-                \PhpOffice\PhpSpreadsheet\NamedRange::createFromRange(
-                    'buildings',
-                    $dataSheet,
-                    'B2:B50'
-                )
-            );
-
-            // Vérification debug
-            \Log::info('Named ranges:', $spreadsheet->getNamedRanges());
-        }
-    }
-
-    // public function registerEvents(): array
-    // {
-    //     \Log::info('registerEvents');
-    //     return [
-    //         AfterSheet::class => function (AfterSheet $event) {
-                
-    //         }
-    //     ];
-    // }
 }
