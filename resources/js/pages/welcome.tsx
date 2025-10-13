b9c1640f895e42997396ef94231108288322ba8e
Crolweb <crolweb@gmail.com>
2025-10-13 13:50:13 +0200

    change welcome page

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,35 +1,12 @@
-import WebsiteLayout from '@/layouts/website-layout';
+import { Button } from '@/components/ui/button';
 
 export default function Welcome() {
-
     return (
-        <WebsiteLayout>
-            <section className="py-20">
-                <div className="mx-auto flex gap-30">
-                    <div className="flex max-w-lg flex-col items-center justify-center gap-10">
-                        <p className="text-4xl font-semibold">Et si la gestion de vos installations devenait facile?</p>
-                        <h1 className="!text-lg">SME-Facility booste la productivité et la croissance de votre entreprise.</h1>
-                        <p>
-                            Véritable outil de Facility management, SME-Facility centralise l'information sur les équipements, simplifie votre
-                            quotidien et renforce la collaboration{' '}
-                        </p>
-                    </div>
-                    <img src="images/home/fm_sm.jpg" alt="" className="blob max-h-72 w-auto shadow-2xl rounded-md" />
-                </div>
-            </section>
-            <section className="py-20">
-                <div className="mx-auto flex gap-30">
-                    <div className="flex max-w-lg flex-col items-center justify-center gap-10">
-                        <p className="text-4xl font-semibold">Et si la gestion de vos installations devenait facile?</p>
-                        <h1 className="!text-lg">SME-Facility booste la productivité et la croissance de votre entreprise.</h1>
-                        <p>
-                            Véritable outil de Facility management, SME-Facility centralise l'information sur les équipements, simplifie votre
-                            quotidien et renforce la collaboration{' '}
-                        </p>
-                    </div>
-                    <img src="images/home/fm_sm.jpg" alt="" className="blob h-64 w-auto shadow-2xl" />
-                </div>
-            </section>
-        </WebsiteLayout>
+        <div className="bg-logo flex h-screen w-screen flex-col items-center justify-center">
+            <img src="images/logo.png" alt="" className="w-96" />
+            <a href="login">
+                <Button>Login</Button>
+            </a>
+        </div>
     );
 }
