c3cbf2a6e236bd52b966297b5833bdbd5b1f853a
Crolweb <crolweb@gmail.com>
2025-10-15 18:13:03 +0200

    change layout login

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,5 +1,4 @@
 import AppLogo from '@/components/app-logo';
-import AppLogoIcon from '@/components/app-logo-icon';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
@@ -11,12 +10,12 @@ interface AuthLayoutProps {
 
 export default function AuthSimpleLayout({ children, title, description }: PropsWithChildren<AuthLayoutProps>) {
     return (
-        <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 w-full">
-            <div className="max-w-sm ">
+        <div className="bg-background flex min-h-svh w-full flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div className="max-w-sm">
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
+                            <div className="mb-1 flex w-36 items-center justify-center rounded-md">
                                 <AppLogo />
                             </div>
                             <span className="sr-only">{title}</span>
