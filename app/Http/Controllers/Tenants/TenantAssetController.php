5c4c908ea7926b644e31aa0c3299032bb7acd4bf
Crolweb <crolweb@gmail.com>
2025-11-19 13:37:31 +0100

    fix: dashboardcontroller - show interventions only if Asset not trashed

--- app/Http/Controllers/Tenants/TenantAssetController.php
+++ app/Http/Controllers/Tenants/TenantAssetController.php
@@ -100,6 +100,8 @@ class TenantAssetController extends Controller
             abort(403);
 
         $asset = Asset::where('reference_code', $asset->reference_code)->with(['maintainable.manager:id,first_name,last_name', 'contracts:id,name,type,provider_id,status,renewal_type,end_date,internal_reference,provider_reference', 'contracts.provider:id,name,logo', 'maintainable.providers:id,name'])->first();
+
+
         return Inertia::render('tenants/assets/ShowAsset', ['item' => $asset->append('level_path')]);
     }
 
